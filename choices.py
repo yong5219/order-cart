9a8ac2bd4145ded62bdfadf698b253b58b35523e
Ting Jia Yong <jiayong@lively.com.my>
2015-11-13 01:24:15 +0800

    Create choices.py

---
+++ choices.py
@@ -0,0 +1,11 @@
+ORDER_STATUSES = (
+    (ORDER_UNCONFIRMED, 'Unconfirmed'),
+    (ORDER_CONFIRMED, 'In Order Cart (Unpaid)'),
+    (ORDER_PAID, 'Paid'),
+    (ORDER_CANCELLED, 'Cancelled'),
+    (ORDER_IN_PROGRESS, 'Printing in Progress'),
+    (ORDER_ERROR, 'Error'),
+    (ORDER_ATTEMPT_TO_DELIVER, 'Attempt Delivering'),
+    (ORDER_DELIVERED, 'Delivered'),
+    (ORDER_READLY_TO_PICK_UP, 'Ready To Pick Up'),
+)
